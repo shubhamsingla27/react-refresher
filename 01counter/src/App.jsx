c2d694003c1fbcca0faf35d0d31f6648c013ded4
Shubham Singla <shubhamsingla@BBMacBook-Pro.local>
2025-02-12 18:37:22 -0800

    Reimplemented 01counter

--- 01counter/src/App.jsx
+++ 01counter/src/App.jsx
@@ -2,22 +2,27 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [counter, setCounter]= useState(0)
+  const [counter, setCounter]=useState(0)
 
-  const decre = () => {
-    if (counter>0){
-      setCounter(counter-1)
-    }
+  const handleIncrement=()=>{
+    setCounter((prevCounter)=>prevCounter+1)
   }
 
+  const handleDecrement=()=>{
+    setCounter((prevCounter)=>{
+      if (prevCounter===0) return 0
+      return prevCounter-1
+    })
+  }
+  
   return (
     <>
-      <h1>Hello</h1>
-      <h1>Counter Value:{counter}</h1>
-
-      <button onClick={()=>setCounter(counter+1)}>Increment</button>
-      <button onClick={()=>decre()}>Decrement</button>
+    <h1>Hello</h1>
+    <h2>{`Counter value is ${counter}`}</h2>
+    <button onClick={handleIncrement}>Increment</button>
+    <button onClick={handleDecrement}>Decrement</button>
     </>
+
   )
 }
 
